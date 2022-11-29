03948f5ec63c6e328b51b4d687235b72fbcea665
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2022-11-29 17:14:29 +0000

    initial commit of main files

---
+++ src/questions.js
@@ -0,0 +1,77 @@
+const StartQuestion =[
+    {   type: 'list',
+        message: 'What would you like to do?',
+        name: 'selection',
+        choices: ['Add a Manager', 'Add an Engineer', 'Add an Intern', 'Build Team Page'],
+      },
+];
+
+const ManagerQues =[
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Employee's Name?",
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Please provide the Employee's ID number",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide the Employee's email address",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please provide the Managers office telephone number",
+    },
+];
+
+const EngineerQues =[
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Employee's Name?",
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Please provide the Employee's ID number",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide the Employee's email address",
+    },
+    {
+        type: "input",
+        name: "GitHub",
+        message: "Please provide the Eningeer's GitHub username",
+    },
+];
+
+const InternQues =[
+    {
+        type: "input",
+        name: "name",
+        message: "What is the Employee's Name?",
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Please provide the Employee's ID number",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide the Employee's email address",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Please provide the Intern's School name",
+    },
+];
+
